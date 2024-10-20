95c6beeacbda936958a9a75d606db5392e65140e
Jenny Honkavaara <144600573+honjen@users.noreply.github.com>
2024-10-20 23:58:08 +0300

    Add search function, csrf token check

--- routes.py
+++ routes.py
@@ -1,169 +1,231 @@
 from app import app
-from flask import render_template, request, redirect, url_for
-import messages, users
+from flask import render_template, request, redirect, abort
+import messages, users, re
 
 @app.route("/")
 def index():
     admin = users.check_admin()
+    list_users = users.get_users()
     list_categories = messages.get_categories()
     return render_template("index.html", categories=list_categories,
-                           count_categories=len(list_categories), admin=admin, current_page='index')
+                                        count_categories=len(list_categories), 
+                                        admin=admin, 
+                                        users=list_users, 
+                                        current_page="index")
 
-@app.route("/new_category", methods=["POST"])
+
+@app.route("/new_category", methods=["GET", "POST"])
 def new_category():
+    if users.csrf_token() != request.form["csrf_token"]:
+        abort(403)
     category_name = request.form["category_name"]
     if messages.check_categories(category_name):
-        return render_template("error.html", message="nimi ei saa olla sama")
-    is_secret = request.form["is_secret"]
-    if is_secret == "1":
-        is_secret == True
+        return render_template("error.html", message="saman niminen keskustelualue on jo olemassa")
+    if not messages.check_category_title(category_name):
+        return render_template("error.html", message="1-50 merkkiä pitkä nimi ei saa sisältää / merkkiä tai pelkkiä välilyöntejä")
+    option = request.form["is_secret"]
+    if option == "0":
+        is_secret = False
     else:
-        is_secret == False
+        is_secret = True
     if messages.create_category(category_name, is_secret):
         return redirect("/")
     else:
-        return render_template("error.html", message="alueen luonti ei onnistunut")
-
+        return render_template("error.html", message="keskustelualuetta ei voitu luoda")
+    
+@app.route("/edit_category", methods=["POST"])
+def edit_category():
+    if users.csrf_token() != request.form["csrf_token"]:
+        abort(403)
+    category_id = request.form["category_id"]
+    category_name = request.form["category_name"]
+    option = request.form["is_secret"]
+    allowed_users = request.form.getlist("user_ids")
+    allowed_users = [int(user_id) for user_id in allowed_users]
+    if messages.check_categories_unique(category_name, category_id):
+        return render_template("error.html", message="saman niminen keskustelualue on jo olemassa")
+    if not messages.check_category_title(category_name):
+        return render_template("error.html", message="1-50 merkkiä pitkä nimi ei saa sisältää / merkkiä")
+    if option == "0":
+        is_secret = False
+    else:
+        is_secret = True
+    if messages.edit_category(category_id, category_name, is_secret, allowed_users):
+        return redirect("/")
+    return render_template("error.html", message="keskustelualueen päivitys epäonnistui")
+    
 @app.route("/delete_category", methods=["POST"])
 def delete_category():
+    if users.csrf_token() != request.form["csrf_token"]:
+        abort(403)
     category_id = request.form["category_id"]
     if messages.delete_category(category_id):
         return redirect("/")
     else:
-        return render_template("error.html", message="alueen poisto ei onnistunut")
+        return render_template("error.html", message="keskustelualueen poisto epäonnistui")
 
 @app.route("/<category_name>")
 def category_page(category_name):
     category = messages.check_categories(category_name)
     if category is None or not users.check_rights(category[1]):
         return redirect("/")
+    if category[2] == True and not users.user_id():
+        return redirect("/")
     list_threads = messages.get_threads(category[1])
-    return render_template("category.html", 
-                           category=category, 
-                           threads=list_threads,
-                           count_threads=len(list_threads), 
-                           current_page='category')
-
+    return render_template("category.html", category=category, 
+                                            threads=list_threads,
+                                            count_threads=len(list_threads),
+                                            current_page="category")
+                            
 @app.route("/<category_name>/new_thread", methods=["POST"])
 def new_thread(category_name):
+    if users.csrf_token() != request.form["csrf_token"]:
+        abort(403)
     category_name = request.form["category_name"]
     category_id = request.form["category_id"]
     thread_name = request.form["thread_name"]
     content = request.form["content"]
-    thread_id = messages.create_thread(category_id, thread_name)
-    if messages.send_message(content, category_id, thread_id):
-        return redirect(f"/{category_name}/{thread_id}")
+    if messages.check_thread_title(thread_name) and messages.check_message(content):
+        thread_id = messages.create_thread(content, category_id, thread_name)
+        if thread_id:    
+            return redirect(f"/{category_name}/{thread_id}")
+        else:
+            return render_template("error.html", message="ketjun luonti epäonnistui")
     else:
-        return render_template("error.html", message="ketjun luonti ei onnistunut")
+        return render_template("error.html", message="otsikon pituus saa olla 1-100 merkkiä ja viestin pituus saa olla 1-20000 merkkiä")
 
 @app.route("/<category_name>/edit_thread", methods=["POST"])
 def edit_thread(category_name):
+    if users.csrf_token() != request.form["csrf_token"]:
+        abort(403)
     category_name = request.form["category_name"]
     thread_id = request.form["thread_id"]
     thread_name = request.form["thread_name"]
-    if messages.edit_thread(thread_name, thread_id):
-        return redirect(f"/{category_name}")
+    content = request.form["content"]
+    if messages.check_thread_title(thread_name) and messages.check_message(content):
+        if messages.edit_thread(content, thread_name, thread_id):
+            return redirect(f"/{category_name}")
+        else:
+            return render_template("error.html", message="ketjun muokkaus epäonnistui")
     else:
-        return render_template("error.html", message="ketjun luonti ei onnistunut")
+        return render_template("error.html", message="otsikon pituus saa olla 1-100 merkkiä ja viestin pituus saa olla 1-20000 merkkiä")
     
 @app.route("/<category_name>/delete_thread", methods=["POST"])
 def delete_thread(category_name):
+    if users.csrf_token() != request.form["csrf_token"]:
+        abort(403)
     thread_id = request.form["thread_id"]
     if messages.delete_thread(thread_id):
         return redirect(f"/{category_name}")
     else:
-        return render_template("error.html", message="ketjun poisto ei onnistunut")
+        return render_template("error.html", message="ketjun poisto epäonnistui")
 
 @app.route("/<category_name>/<int:thread_id>")
 def thread_page(category_name, thread_id):
     category = messages.check_categories(category_name)
+    if category is None:
+        return redirect("/")
     thread = messages.check_threads(thread_id, category[1])
     list_messages = messages.get_messages(thread_id)
-    if category is None or thread is None:
+    if thread is None or not users.check_rights(category[1]):
         return redirect("/")
-    return render_template("thread.html", 
-                           category=category, 
-                           thread=thread, 
-                           messages=list_messages,
-                           count_messages=len(list_messages), 
-                           current_page='thread')
+    if category[2] == True and not users.user_id():
+        return redirect("/")
+    return render_template("thread.html", category=category,
+                                        thread=thread,
+                                        messages=list_messages,
+                                        count_messages=len(list_messages),
+                                        current_page="thread")
 
 @app.route("/<category_name>/<int:thread_id>/send_message", methods=["POST"])
 def send_message(category_name, thread_id):
+    if users.csrf_token() != request.form["csrf_token"]:
+        abort(403)
     category_id = request.form["category_id"]
     thread_id = request.form["thread_id"]
     content = request.form["content"]
     if messages.send_message(content, category_id, thread_id):
         return redirect(f"/{category_name}/{thread_id}")
     else:
-        return render_template("error.html", 
-                               message="viestin lähetys ei onnistunut", 
-                               current_page='error')
-
+        return render_template("error.html", message="viestin pituus saa olla 1-20000 merkkiä")
+    
 @app.route("/<category_name>/<int:thread_id>/edit_message", methods=["POST"])
 def edit_message(category_name, thread_id):
+    if users.csrf_token() != request.form["csrf_token"]:
+        abort(403)
     thread_id = request.form["thread_id"]
     content = request.form["content"]
     message_id = request.form["message_id"]
     if messages.edit_message(content, message_id):
         return redirect(f"/{category_name}/{thread_id}")
     else:
-        return render_template("error.html", message="viestin lähetys ei onnistunut", current_page='error')
+        return render_template("error.html", message="viestin pituus saa olla 1-20000 merkkiä")
     
 @app.route("/<category_name>/<int:thread_id>/delete_message", methods=["POST"])
 def delete_message(category_name, thread_id):
+    if users.csrf_token() != request.form["csrf_token"]:
+        abort(403)
     message_id = request.form["message_id"]
     if messages.delete_message(message_id):
         return redirect(f"/{category_name}/{thread_id}")
     else:
-        return render_template("error.html", message="viestin poisto ei onnistunut", current_page='error')
+        return render_template("error.html", message="viestin poisto epäonnistui")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    redirect_url = request.args.get('redirect', url_for('index'))
     if request.method == "GET":
-        return render_template("login.html", 
-                               redirect_url=redirect_url, 
-                               current_page='login')
+        return render_template("login.html", current_page="login")
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        redirect_url = request.form.get('redirect_url', url_for('index'))
         if users.login(username, password):
-            return redirect(redirect_url)
+            return redirect("/")
         else:
-            return render_template("error.html", 
-                                   message="väärä tunnus tai salasana", 
-                                   current_page='error')
+            return render_template("error.html", message="väärä käyttäjätunnus tai salasana")
 
 @app.route("/logout")
 def logout():
-    users.logout()
+    if users.user_id():
+        users.logout()
     return redirect("/")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    redirect_url = request.args.get('redirect', url_for('index'))
     if request.method == "GET":
-        return render_template("register.html", 
-                               redirect_url=redirect_url, 
-                               current_page='register')
+        return render_template("register.html", current_page="register")
     if request.method == "POST":
         username = request.form["username"]
         password1 = request.form["password1"]
         password2 = request.form["password2"]
-        redirect_url = request.form.get('redirect_url', url_for('index'))
-        if len(username) < 1 or " " in username:
-            return redirect("/register")
-        if len(password1) < 1 or " " in password1:
-            return redirect("/register")
+        if not re.match(r"^\S{1,32}$", username):
+            return render_template("error.html", message="käyttäjätunnuksen pituus saa olla 1-32 merkkiä")
+        if users.check_username(username):
+            return render_template("error.html", message="käyttäjätunnus on jo olemassa")
+        if not re.match(r"^\S{1,64}$", password1):
+            return render_template("error.html", message="salasanan pituus saa olla 1-64 merkkiä")
         if password1 != password2:
-            return render_template("error.html", 
-                                   message="salasanat eroavat", 
-                                   current_page='error')
+            return render_template("error.html", message="salasanat ovat erilaiset")
         if users.register(username, password1):
-            return redirect(redirect_url)
+            return redirect("/")
         else:
-            return render_template("error.html", 
-                                   message="rekisteröinti ei onnistunut", 
-                                   current_page='error')
+            return render_template("error.html", message="kaikki kentät pitää täyttää oikein")
+        
+@app.route("/search")
+def search():
+    return render_template("search.html", current_page="search")
+
+@app.route("/searchresults")
+def searchresults():
+    query = request.args["query"]
+    options = request.args.getlist("search_options")
+    categories_result = messages.search_categories(query)
+    threads_result = messages.search_threads(query)
+    messages_result = messages.search_messages(query)
+    if messages.check_message(query) and options != []:
+        return render_template("searchresults.html", query=query,
+                                                    options=options,
+                                                    categories_result=categories_result, 
+                                                    threads_result=threads_result,
+                                                    messages_result=messages_result, 
+                                                    current_page="searchresults")
+    return redirect("/search")
--- users.py
+++ users.py
@@ -2,9 +2,15 @@ from db import db
 from sqlalchemy.sql import text
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
+import secrets
+
 
 def login(username, password):
-    sql = "SELECT u.user_id, u.password, r.is_admin FROM users u LEFT JOIN rights r ON u.user_id = r.user_id WHERE u.username = :username"
+    sql = """
+    SELECT u.user_id, u.password 
+    FROM users u 
+    WHERE u.username = :username
+    """
     result = db.session.execute(text(sql), {"username":username})
     user = result.fetchone()
     if not user:
@@ -12,13 +18,16 @@ def login(username, password):
     else:
         if check_password_hash(user.password, password):
             session["user_id"] = user.user_id
-            session["is_admin"] = user.is_admin
+            session["csrf_token"] = secrets.token_hex(16)
             return True
         else:
             return False
 
+
 def logout():
     del session["user_id"]
+    del session["csrf_token"]
+
 
 def register(username, password):
     hash_value = generate_password_hash(password)
@@ -30,17 +39,63 @@ def register(username, password):
         return False
     return login(username, password)
 
+
 def user_id():
     return session.get("user_id",0)
 
+
 def check_rights(category_id):
-    sql = "SELECT CASE WHEN r.is_admin THEN TRUE WHEN c.is_secret AND :category_id = ANY(r.secret_areas) THEN TRUE WHEN c.is_secret IS FALSE THEN TRUE ELSE FALSE END AS has_access FROM rights r JOIN categories c ON c.category_id = :category_id WHERE r.user_id = :user_id OR :user_id = 0"
+    sql = """
+    SELECT CASE 
+    WHEN r.is_admin THEN TRUE
+    WHEN c.is_secret AND :user_id = ANY(c.allowed_users) THEN TRUE
+    WHEN c.is_secret IS FALSE THEN TRUE
+    ELSE FALSE
+    END AS has_access
+    FROM rights r
+    JOIN categories c ON c.category_id = :category_id
+    WHERE r.user_id = :user_id OR :user_id = 0
+    """
     result = db.session.execute(text(sql), {'user_id': user_id(), 'category_id': category_id})
     return result.fetchone()[0]
 
 def check_admin():
-    if user_id == 0 or not user_id():
+    if user_id() == 0 or not user_id():
         return False
-    sql = "SELECT CASE WHEN r.is_admin THEN TRUE  ELSE FALSE END AS is_admin FROM rights r WHERE r.user_id = :user_id OR :user_id = 0"
+    sql = """
+    SELECT CASE 
+    WHEN r.is_admin THEN TRUE 
+    ELSE FALSE 
+    END AS is_admin 
+    FROM rights r 
+    WHERE r.user_id = :user_id OR :user_id = 0
+    """
     result = db.session.execute(text(sql), {'user_id': user_id()})
+    if result == None:
+        return False
     return result.fetchone()[0]
+
+
+def get_users():
+    sql = """
+    SELECT u.user_id, u.username 
+    FROM users u
+    JOIN rights r ON u.user_id = r.user_id 
+    WHERE r.is_admin IS FALSE
+    """
+    result = db.session.execute(text(sql))
+    return result.fetchall()
+
+
+def csrf_token():
+    return session.get("csrf_token")
+
+
+def check_username(username):
+    sql = """
+    SELECT username 
+    FROM users 
+    WHERE username = :username
+    """
+    result = db.session.execute(text(sql), {"username":username})
+    return result.fetchone()
